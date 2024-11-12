b2f36dd1b20e5e4187e6a10d7cb3e5ace8141055
irakligudiashvili <54039911+irakligudiashvili@users.noreply.github.com>
2024-11-12 21:35:19 +0400

    added Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import { useCart } from "../context/CartContext";
+
+function Navbar(){
+    const {getCartItemCount } = useCart();
+
+    return (
+        <nav>
+            <h1>Phone Store</h1>
+            <div>
+                <Link to='/'>Home</Link>
+                <Link to='/shop'></Link>
+            </div>
+            <div>
+                <Link to='/cart'>Cart {getCartItemCount()}</Link>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar

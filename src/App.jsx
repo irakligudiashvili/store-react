3ffe0a7810350f00f423a0b8bc67420ae2c0ebae
irakligudiashvili <54039911+irakligudiashvili@users.noreply.github.com>
2024-11-12 21:24:33 +0400

    added CartContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import HomePage from './pages/HomePage'
 import './styles/App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import HomePage from './pages/HomePage'
+import ShopPage from './pages/ShopPage'
+import { CartProvider } from './context/CartContext'
 
 function App() {
-
   return (
-    <HomePage />
-  )
+    <CartProvider>
+      <Router>
+        <Navbar />
+        <main>
+          <Routes>
+            <Route index element={<HomePage />} />
+            <Route path="/shop" element={<ShopPage />} />
+          </Routes>
+        </main>
+      </Router>
+    </CartProvider>
+  );
 }
 
 export default App

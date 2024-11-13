e8b0b6c9b90fd81c8dbea30ffd72aab74872dd6d
irakligudiashvili <54039911+irakligudiashvili@users.noreply.github.com>
2024-11-13 18:47:27 +0400

    added header

--- src/partials/Header.jsx
+++ src/partials/Header.jsx
@@ -1,6 +1,20 @@
+import { Link } from "react-router-dom"
+import './header.css'
+
 function Header(){
     return (
-        <h1>Header</h1>
+        <nav className='header__nav'>
+            <Link to='/' className='header__logo'>
+                <img src="src/assets/logo.png"/>
+            </Link>
+            <ul className='header__menu'>
+                <li className='header__item'><Link to='/men'>Men's Clothing</Link></li>
+                <li className='header__item'><Link to='/women'>Women's Clothing</Link></li>
+                <li className='header__item'><Link to='/jewelry'>Jewelry</Link></li>
+                <li className='header__item'><Link to='/electronics'>Electronics</Link></li>
+                <li className='header__item header__item-cart'><Link to='/cart'>Cart: 0</Link></li>
+            </ul>
+        </nav>
     )
 }
 

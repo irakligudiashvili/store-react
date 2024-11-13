603f062602c0fb17357a7d1ab08a8e7e4460dde0
irakligudiashvili <54039911+irakligudiashvili@users.noreply.github.com>
2024-11-13 17:37:03 +0400

    restructured project

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,8 @@
-import './styles/App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import HomePage from './pages/HomePage'
-import ShopPage from './pages/ShopPage'
-import { CartProvider } from './context/CartContext'
-import CartPage from './pages/CartPage'
+import './App.css'
 
 function App() {
   return (
-    <CartProvider>
-      <Router>
-        <Navbar />
-        <main>
-          <Routes>
-            <Route index element={<HomePage />} />
-            <Route path="/shop" element={<ShopPage />} />
-            <Route path="/cart" element={<CartPage />} />
-          </Routes>
-        </main>
-      </Router>
-    </CartProvider>
+    <h1>App</h1>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import routes from './routes.jsx'
+
+const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={router} />
+  </StrictMode>
 )

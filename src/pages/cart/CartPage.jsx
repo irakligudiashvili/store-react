261da60558002e4ce229ea3b4a49b71118484edc
irakligudiashvili <54039911+irakligudiashvili@users.noreply.github.com>
2024-11-18 03:28:35 +0400

    added cart page styling

--- src/pages/cart/CartPage.jsx
+++ src/pages/cart/CartPage.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { useCart } from "../../contexts/CartProvider"
 import './cartpage.css';
 
@@ -7,29 +8,58 @@ function CartPage(){
     const { removeItem, adjustCount, getProductTotalPrice, getTotalPrice } = useCart();
 
     return (
-        <div>
-            <h1>Cart Page</h1>
+        <div className="cart-page">
+            <h1 className="cart-page__heading">Your Cart</h1>
             {cartItems.length === 0 ? (
-                <p>Your cart is empty</p>
+                <>
+                    <p>No items in cart</p>
+                    <Link to='/'>Return to Homepage</Link>
+                </>
             ): (
-                <div>
-                    <ul>
+                <div className="checkout">
+                    <ul className="checkout__cart">
                         {cartItems.map(item => (
-                            <li key={item.id}>
-                                <img src={item.image} />
-                                <h2>{item.title}</h2>
-                                <p>Price: ${item.price}</p>
-                                <p>Quantity: {item.quantity}</p>
-                                <button onClick={() => adjustCount(item.id, 'decrease')}>-</button>
-                                <button onClick={() => removeItem(item.id)}>Remove Item</button>
-                                <button onClick={() => adjustCount(item.id, 'increase')}>+</button>
-                                <p>Total: ${getProductTotalPrice(item.id)}</p>
+                            <li key={item.id} className="checkout__cart-item">
+                                <img src={item.image} className="checkout__cart-item-img" />
+                                <div className="checkout__cart-item-info">
+                                    <div>
+                                        <h2 className="checkout__cart-item-title">{item.title}</h2>
+                                        <p className="checkout__cart-item-price">Price: ${item.price}</p>
+                                    </div>
+                                    <p>Total: ${getProductTotalPrice(item.id)}</p>
+                                </div>
+                                <div className="checkout__cart-actions">
+                                    <button className="checkout__cart-remove" onClick={() => removeItem(item.id)}>Remove Item</button>
+                                    <div>
+                                        <p className="checkout__cart-item-quantity">Quantity: {item.quantity}</p>
+                                        <button className="checkout__cart-item-btn" onClick={() => adjustCount(item.id, 'decrease')}>-</button>
+                                        <button className="checkout__cart-item-btn" onClick={() => adjustCount(item.id, 'increase')}>+</button>
+                                    </div>
+                                </div>
                             </li>
                         ))}
                     </ul>
 
-
-                    <p>Total: ${getTotalPrice()}</p>
+                    <div className="checkout__summary">
+                        <ul className="checkout__summary-list">
+                            {cartItems.map(item => (
+                                <li key={item.id} className="checkout__summary-item">
+                                    <div>
+                                        <p className="checkout__summary-item-title">{item.title}</p>
+                                        <p className="checkout__summary-item-quantity">x{item.quantity}</p>
+                                    </div>
+                                    <p className="checkout__summary-item-price">${getProductTotalPrice(item.id)}</p>
+                                </li>
+                            ))}
+                        </ul>
+                        <div>
+                            <div className="checkout__summary-total">
+                                <p className="checkout__summary-tota-text">Total:</p>
+                                <p className="checkout__summary-total-price">${getTotalPrice()}</p>
+                            </div>
+                            <button className="checkout__summary-btn">Checkout</button>
+                        </div>
+                    </div>
                 </div>
             )} 
 

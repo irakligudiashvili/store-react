748e1f419b5a7fc99d7d2166e4e9a53a601d9a75
irakligudiashvili <54039911+irakligudiashvili@users.noreply.github.com>
2024-11-25 22:22:14 +0400

    made burger menu cover screen and disabled scroll while open

--- src/partials/Header.jsx
+++ src/partials/Header.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom"
 import './header.css'
 import { useCart } from "../contexts/CartProvider";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Header(){
 
@@ -12,17 +12,29 @@ function Header(){
         setMenuOpen(!menuOpen);
     }
 
+    const closeMenu = () => {
+        setMenuOpen(false);
+    }
+
+    useEffect(() => {
+        if (menuOpen) {
+            document.body.classList.add('scroll-lock');
+        } else {
+            document.body.classList.remove('scroll-lock');
+        }
+    }, [menuOpen]);
+
     return (
         <nav className='header__nav'>
             <Link to='/' className='header__logo'>
                 <img src="/src/assets/logo.png"/>
             </Link>
             <ul className={`header__menu ${menuOpen ? 'header__menu-open' : ''}`}>
-                <li className='header__item'><Link to='/category/men'>Men's Clothing</Link></li>
-                <li className='header__item'><Link to='/category/women'>Women's Clothing</Link></li>
-                <li className='header__item'><Link to='/category/jewelery'>Jewelry</Link></li>
-                <li className='header__item'><Link to='/category/electronics'>Electronics</Link></li>
-                <li className='header__item header__item-cart'><Link to='/cart'>Cart: {getTotalItemCount()}</Link></li>
+                <li className='header__item' onClick={closeMenu}><Link to='/category/men'>Men's Clothing</Link></li>
+                <li className='header__item' onClick={closeMenu}><Link to='/category/women'>Women's Clothing</Link></li>
+                <li className='header__item' onClick={closeMenu}><Link to='/category/jewelery'>Jewelry</Link></li>
+                <li className='header__item' onClick={closeMenu}><Link to='/category/electronics'>Electronics</Link></li>
+                <li className='header__item header__item-cart' onClick={closeMenu}><Link to='/cart'>Cart: {getTotalItemCount()}</Link></li>
             </ul>
             <button className="header__burger" onClick={toggleMenu}>
                 ☰
